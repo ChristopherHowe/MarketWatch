66627773bd6447293e8cac2af163b99cc7779adf
ChristopherHowe <howechristopher5@gmail.com>
2023-05-11 14:30:37 -0700

    adding run script without build

--- env.sh
+++ env.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #responsible for setting all necessary environment variables.
 export API_DOMAIN=https://api.twelvedata.com
-export TWELVE_KEY=$(cat ~/personal/twelve-data-token)
+#export TWELVE_KEY=$(cat ~/personal/twelve-data-token)
---
+++ run-no-make.sh
@@ -0,0 +1,17 @@
+cd build
+source ../env.sh
+
+if [ -z $TWELVE_KEY ]; 
+    then
+    echo failed to load in stock API key, make sure twelve-data-token is in ~/personal directory
+    echo exiting...
+    exit
+fi
+if [ -z $API_DOMAIN ]; 
+    then
+    echo failed to load in API Domain, make sure it is specified in the env.sh file
+    echo exiting...
+    exit
+fi
+
+app/marketwatch
